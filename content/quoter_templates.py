3aed35989236032490f540b1d0b8ef83741a737f
Sherida de Koning <136692752+shamijla1@users.noreply.github.com>
2024-09-04 13:09:23 +0200

    orgineel

--- content/quoter_templates.py
+++ content/quoter_templates.py
@@ -60,7 +60,7 @@ def page(content,user_id,title,error=None):
 """
     else:
         links = f"""
-<label class="link" for="signinCheckbox">523781</label>
+<label class="link" for="signinCheckbox">Inloggen</label>
 """
 
     return f"""<!DOCTYPE html>
