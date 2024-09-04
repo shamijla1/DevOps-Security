98d63f6f25ce7060a405650672c061930a7c0c7c
Sherida de Koning <136692752+shamijla1@users.noreply.github.com>
2024-09-04 13:06:21 +0200

    inloggen

--- content/quoter_templates.py
+++ content/quoter_templates.py
@@ -60,13 +60,13 @@ def page(content,user_id,title,error=None):
 """
     else:
         links = f"""
-<label class="link" for="signinCheckbox">Inloggen</label>
+<label class="link" for="signinCheckbox">523781</label>
 """
 
     return f"""<!DOCTYPE html>
 <html lang="en-US">
 <head>
-  <title>{title or "Quoter XP Sherida"}</title>
+  <title>{title or "Quoter XP"}</title>
   <meta charset="utf-8">
   <link rel="stylesheet" type="text/css" href="/static/style.css">
 </head>
